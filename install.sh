2846d4830f7d7821a5a80c05b4ac5259ab7163b3
johnnyrodriguezdk <johnnyrodriguez245@gmail.com>
2025-11-19 02:04:03 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ exit 0
 }
 
 ubuntuV=$(lsb_release -r | awk '{print $2}' | cut -d. -f1)
-
 [[ $(($ubuntuV < 20)) = 1 ]] && {
 clear
 echo "¡POR FAVOR, INSTALA EN UBUNTU 20.04 O 22.04! EL TUYO ES $ubuntuV"
@@ -18,25 +17,27 @@ echo
 rm /root/install.sh
 exit 0
 }
+
 [[ -e /root/paineldtunnel/src/index.ts ]] && {
   clear
   echo "EL PANEL YA ESTÁ INSTALADO. ¿DESEAS ELIMINARLO? (s/n)"
   read remo
   [[ $remo = @(s|S) ]] && {
-  cd /root/paineldtunnel
-  rm -r painelbackup > /dev/null
-  mkdir painelbackup > /dev/null
-  cp prisma/database.db painelbackup
-  cp .env painelbackup
-  zip -r painelbackup.zip painelbackup
-  mv painelbackup.zip /root
-  rm -r /root/paineldtunnel
-  rm /root/install.sh
-  echo "¡Eliminado con éxito!"
-  exit 0
+    cd /root/paineldtunnel
+    rm -r painelbackup > /dev/null
+    mkdir painelbackup > /dev/null
+    cp prisma/database.db painelbackup
+    cp .env painelbackup
+    zip -r painelbackup.zip painelbackup
+    mv painelbackup.zip /root
+    rm -r /root/paineldtunnel
+    rm /root/install.sh
+    echo "¡Eliminado con éxito!"
+    exit 0
   }
   exit 0
 }
+
 clear
 echo "¿QUÉ PUERTO DESEAS ACTIVAR?"
 read porta
@@ -44,25 +45,20 @@ echo
 echo "Instalando Panel Dtunnel Mod..."
 echo
 sleep 3
-#========================
+
+# ========================
 sudo apt-get update -y
-sudo apt-get update -y
-sudo apt-get install wget -y
-sudo apt-get install curl -y
-sudo apt-get install zip -y
-sudo apt-get install npm -y /dev/null
-npm install pm2 -g /dev/null
-sudo apt-get install cron -y
-sudo apt-get install unzip -y
-sudo apt-get install screen -y
-sudo apt-get install git -y
-curl -s -L https://github.com/johnnyrodriguezdk/paineldtunnel/main/setup_20.x | bash
+sudo apt-get install wget curl zip npm cron unzip screen git -y
+npm install pm2 -g
+curl -s -L https://deb.nodesource.com/setup_20.x | bash -
 sudo apt-get install nodejs -y
-#=========================
-git clone https://github.com/johnnyrodriguezdk/paineldtunnel.git
-cd /root/paineldtunnel 
+# ========================
+
+git clone https://github.com/johnnyrodriguezdk/paineldtunnel.git /root/paineldtunnel
+cd /root/paineldtunnel
 chmod 777 pon poff menudt backmod
 mv pon poff menudt backmod /bin
+
 echo "PORT=$porta" > .env
 echo "NODE_ENV=\"producción\"" >> .env
 echo "DATABASE_URL=\"file:./database.db\"" >> .env
@@ -73,13 +69,13 @@ echo "CSRF_SECRET=\"$token1\"" >> .env
 echo "JWT_SECRET_KEY=\"$token2\"" >> .env
 echo "JWT_SECRET_REFRESH=\"$token3\"" >> .env
 echo "ENCRYPT_FILES=\"7223fd56-e21d-4191-8867-f3c67601122a\"" >> .env
+
 npm install
 npx prisma generate
 npx prisma migrate deploy
 npm run start
-#=========================
+
 clear
-echo
 echo
 echo "¡PANEL DTUNNEL MOD INSTALADO CON ÉXITO!"
 echo "Los Archivos Quedan En La Carpeta /root/paineldtunnel"
@@ -91,7 +87,7 @@ echo -e "\033[1;36mEscribe el comando: \033[1;37mmenudt \033[1;32m(Para acceder 
 echo
 rm /root/install.sh
 pon
-echo -e "\033[1;36mTU PANEL:\033[1;37m http://$IP\033[0m"
+echo -e "\033[1;36mTU PANEL:\033[1;37m http://$IP:$porta\033[0m"
 echo
 echo -ne "\n\033[1;31mPULSA ENTER \033[1;33mPara Regresar \033[1;32mAl Prompt! \033[0m"; read
 cat /dev/null > ~/.bash_history && history -c
